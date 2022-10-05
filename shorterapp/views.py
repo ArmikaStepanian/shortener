e408abfeabe31dac4d2a53a619d7c1ba868d6a94
Stepanian <armikastepanian@gmail.com>
2022-10-04 22:21:00 -0400

    add long url exists in db check

--- shorterapp/views.py
+++ shorterapp/views.py
@@ -14,21 +14,14 @@ def index(request):
 
 
 def shortened_url(request):
-    n = 7
-    random_string = ''.join(random.choices(string.ascii_letters, k=n))
-    while not is_unique_combination(str(random_string)):
-        random_string = ''.join(random.choices(string.ascii_letters, k=n))
+    y = request.POST['long_url_input']
+    if long_url_exist_in_db(y):
+        return response_with_existed_link(request, y)
+    random_string = create_combination()
+    while duplicated_combination(str(random_string)):
+        random_string = create_combination()
     else:
-        x = str(random_string)
-        y = request.POST['long_url_input']
-        link = Link(shortlink=x, longlink=y)
-        link.save()
-        context = {
-            'link': link,
-        }
-
-    template = loader.get_template('shortened_url.html')
-    return HttpResponse(template.render(context, request))
+        return response_with_new_link(request, random_string, y)
 
 
 def redirect_url_view(request, shortened_part):
@@ -43,8 +36,41 @@ def show_help(request):
     return render(request, 'help.html')
 
 
-def is_unique_combination(combination):
+def create_combination():
+    n = 7
+    random_string = ''.join(random.choices(string.ascii_letters, k=n))
+    return str(random_string)
+
+
+def duplicated_combination(combination):
     if Link.objects.filter(shortlink=combination).exists():
-        return False
-    else:
         return True
+    else:
+        return False
+
+
+def long_url_exist_in_db(long_url):
+    if Link.objects.filter(longlink=long_url).exists():
+        return True
+    else:
+        return False
+
+
+def response_with_existed_link(request, long_url):
+    link = Link.objects.get(longlink=long_url)
+    context = {
+        'link': link,
+    }
+    template = loader.get_template('shortened_url.html')
+    return HttpResponse(template.render(context, request))
+
+
+def response_with_new_link(request, random_string, long_url):
+    x = str(random_string)
+    link = Link(shortlink=x, longlink=long_url)
+    link.save()
+    context = {
+        'link': link,
+    }
+    template = loader.get_template('shortened_url.html')
+    return HttpResponse(template.render(context, request))
