60f4bd68e02a67ed8473b39ed91fee7b904636fc
Stepanian <armikastepanian@gmail.com>
2022-10-05 21:53:34 -0400

    add comments

--- shorterapp/views.py
+++ shorterapp/views.py
@@ -9,21 +9,26 @@ from django.template import loader
 from shorterapp.models import Link
 
 
+# Initial application page
 def index(request):
     return render(request, 'index.html')
 
 
+# Short URL creation for requested long URL
 def shortened_url(request):
     y = request.POST['long_url_input']
     if long_url_exist_in_db(y):
+        # return existed short URL from database
         return response_with_existed_link(request, y)
     random_string = create_combination()
     while duplicated_combination(str(random_string)):
         random_string = create_combination()
     else:
+        # return new short URL
         return response_with_new_link(request, random_string, y)
 
 
+# Redirection from short URL to corresponding long URL
 def redirect_url_view(request, shortened_part):
     try:
         link = Link.objects.get(shortlink=shortened_part)
@@ -33,16 +38,19 @@ def redirect_url_view(request, shortened_part):
         raise Http404('<h2>Sorry page not found</h2>')
 
 
+# Help page with instructions how to use service
 def show_help(request):
     return render(request, 'help.html')
 
 
+# Random short URL combination generation
 def create_combination():
     n = 7
     random_string = ''.join(random.choices(string.ascii_letters, k=n))
     return str(random_string)
 
 
+# Check if random short URL combination already exists in database
 def duplicated_combination(combination):
     if Link.objects.filter(shortlink=combination).exists():
         return True
@@ -50,6 +58,7 @@ def duplicated_combination(combination):
         return False
 
 
+# Check if requested long URL already exists in database
 def long_url_exist_in_db(long_url):
     if Link.objects.filter(longlink=long_url).exists():
         return True
@@ -57,6 +66,7 @@ def long_url_exist_in_db(long_url):
         return False
 
 
+# Return existed short URL from database
 def response_with_existed_link(request, long_url):
     link = Link.objects.get(longlink=long_url)
     context = {
@@ -66,6 +76,7 @@ def response_with_existed_link(request, long_url):
     return HttpResponse(template.render(context, request))
 
 
+# Return new short URL
 def response_with_new_link(request, random_string, long_url):
     x = str(random_string)
     link = Link(shortlink=x, longlink=long_url)
@@ -77,6 +88,7 @@ def response_with_new_link(request, random_string, long_url):
     return HttpResponse(template.render(context, request))
 
 
+# Increment redirections and save result in database
 def count_redirections(link):
     link.count = link.count + 1
     link.save()
